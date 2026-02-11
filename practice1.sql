b67f9fae8c6580706e5a572cfc16eef9d4cd986e
m-iftikhar <iftikharrj2001@gmail.com>
2026-02-11 12:28:23 +0500

    basics done

--- practice1.sql
+++ practice1.sql
@@ -11,3 +11,20 @@ id INT AUTO_INCREMENT PRIMARY KEY,
 
 SELECT * FROM users;
 SELECT name ,email FROM users;
+
+RENAME TABLE users TO programers;
+RENAME TABLE programers TO users;
+
+SELECT * FROM users;
+
+ALTER TABLE users ADD COLUMN isactive BOOLEAN DEFAULT TRUE;
+ALTER TABLE users MODIFY COLUMN email VARCHAR(150) ;
+ALTER TABLE users DROP COLUMN isactive;
+
+ALTER TABLE users MODIFY COLUMN email VARCHAR(100) FIRST;
+ALTER TABLE users MODIFY COLUMN gender ENUM('Male', 'Female', 'Other') AFTER name;
+
+INSERT INTO users (name, email, gender, date_of_birth) VALUES
+('Charlie', 'charlie@example.com', 'Other', '1988-02-17'),
+('David', 'david@example.com', 'Male', '2000-08-09'),
+('Eva', 'eva@example.com', 'Female', '1993-12-30');
