534faff36cd05ebbb6146660076b08b08815255b
m-iftikhar <iftikharrj2001@gmail.com>
2026-02-18 17:35:19 +0500

    procedure practice

--- practice4.sql
+++ practice4.sql
@@ -3,3 +3,28 @@
 SHOW INDEXES FROM users;
 CREATE INDEX idx_gender_salary ON users(gender,salary);
 SELECT * FROM users WHERE gender="Male" AND salary="70000";
+
+-- subquery
+
+SELECT name, salary,gender FROM  users WHERE salary > (SELECT AVG(salary) FROM users);
+
+
+-- GROUP BY / HAVING
+
+SELECT gender, AVG(salary) AS avg_salary
+FROM users
+GROUP BY gender
+HAVING AVG(salary) > 60000;
+
+
+-- procedure
+
+USE startersql;
+DELIMITER $$
+CREATE PROCEDURE select_users()
+BEGIN
+SELECT * FROM users;
+END $$
+DELIMITER 
+
+CALL select_users();
